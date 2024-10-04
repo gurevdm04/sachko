b7e59a8bd961ea929a0aa4407a9968af6cd09b04
gurevdm04 <gurevdm04@gmail.com>
2024-10-05 08:53:47 +1000

    Переделан компонент PostList под свой функционал

--- frontend/src/components/PostList/PostList.tsx
+++ frontend/src/components/PostList/PostList.tsx
@@ -1,40 +1,40 @@
 import style from "./PostList.module.scss";
 import { PostCard } from "../PostCard/PostCard";
-import { useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import { fetchPosts } from "../../redux/slices/posts";
 import ContentLoader from "react-content-loader";
 
-export const PostList = () => {
-  const dispatch = useAppDispatch();
-  const userData = useAppSelector((state) => state.auth.data);
-  const { posts } = useAppSelector((state) => state.posts);
+interface PostListProps {
+  isPostLoading: boolean;
+  posts: {
+    items: any;
+  };
+  userData: any;
+}
 
-  const isPostLoading = posts.status === "loading";
-
-  useEffect(() => {
-    dispatch(fetchPosts());
-  }, []);
-
+export const PostList: React.FC<PostListProps> = ({
+  isPostLoading,
+  posts,
+  userData,
+}) => {
   return (
     <>
       <div className={style.wrap}>
-        {(isPostLoading ? [...Array(3)] : posts.items).map((obj, index) =>
-          isPostLoading ? (
-            <Loader key={index} />
-          ) : (
-            <PostCard
-              key={obj._id}
-              id={obj._id}
-              title={obj.title}
-              text={obj.text}
-              updatedAt={obj.updatedAt}
-              user={obj.user}
-              isEditable={userData?._id === obj.user._id}
-              imageUrl={obj.imageUrl}
-              viewsCount={obj.viewsCount}
-            />
-          )
+        {(isPostLoading ? [...Array(3)] : posts.items).map(
+          (obj: any, index: any) =>
+            isPostLoading ? (
+              <Loader key={index} />
+            ) : (
+              <PostCard
+                key={obj._id}
+                id={obj._id}
+                title={obj.title}
+                text={obj.text}
+                updatedAt={obj.updatedAt}
+                user={obj.user}
+                isEditable={userData?._id === obj.user._id}
+                imageUrl={obj.imageUrl}
+                viewsCount={obj.viewsCount}
+              />
+            )
         )}
         {!isPostLoading && posts.items.length === 0 ? <>Постов нет :( </> : null}
       </div>
--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,26 @@
+import { useEffect } from "react";
 import { PostList } from "../../components/PostList/PostList";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { fetchPosts } from "../../redux/slices/posts";
 
 export const HomePage = () => {
+  const dispatch = useAppDispatch();
+  const userData = useAppSelector((state) => state.auth.data);
+  const { posts } = useAppSelector((state) => state.posts);
+
+  const isPostLoading = posts.status === "loading";
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, []);
+
   return (
     <>
-      <PostList />
+      <PostList
+        isPostLoading={isPostLoading}
+        posts={posts}
+        userData={userData}
+      />
     </>
   );
 };
